73c2516dc87b076450ef4d90ebfe6c1c1042e94b
Bernabe Martinez <56056639+BernabeMTpa@users.noreply.github.com>
2020-02-28 09:11:41 -0500

    Carga de Archivos Inicial
    Se suben los archivos de los ejemplos de JavaScript que ya existan en mi pc en el cliente

---
+++ functions.js
@@ -0,0 +1,7 @@
+function mostrarMensaje1(){
+alert('BBBBienvenido al curso JavaScript de aprenderaprogramar.com');
+}
+
+function mostrarMensaje2(){
+alert('HHHHa hecho click sobre el párrafo inferior');
+}
